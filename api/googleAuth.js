d4e798f10c4bf05986aac371881b53970270b111
Reza Bagus Saputra <103319101+RezaBagusS@users.noreply.github.com>
2023-10-29 21:21:23 +0700

    adding response googleAuth

--- api/googleAuth.js
+++ api/googleAuth.js
@@ -2,6 +2,7 @@ const { OAuth2Client } = require("google-auth-library");
 
 const clientId = process.env.CLIENT_ID_DEV;
 const client = new OAuth2Client(clientId);
+const supabase = require("../database/initDB");
 
 const googleAuth = async (req, res) => {
   try {
@@ -14,6 +15,44 @@ const googleAuth = async (req, res) => {
 
     const payload = ticket.getPayload();
     const userId = payload["sub"];
+
+    const user = await supabase
+      .from("allUser")
+      .select("email")
+      .eq("name", payload.name);
+
+    if (user.data.length == 0) {
+      const hashedPassword = await bcrypt.hash("viagoogle", 10);
+
+      const { insertError } = await supabase
+        .from("allUser")
+        .insert([
+          {
+            name: payload.name,
+            email: payload.email,
+            password: hashedPassword,
+          },
+        ]);
+
+      if (insertError) {
+        return res.status(500).json({
+          status: "error",
+          message: "Gagal mendaftarkan pengguna baru",
+        });
+      }
+    } else {
+      const { data: existingUsers, error } = await supabase
+        .from("allUser")
+        .select("email")
+        .eq("email", payload.email);
+
+      if (error) {
+        return res.status(500).json({
+          status: "error",
+          message: "Error saat mengecek daftar pengguna",
+        });
+      }
+    }
 
     res.status(200).json({
       message: "Otentikasi berhasil",
